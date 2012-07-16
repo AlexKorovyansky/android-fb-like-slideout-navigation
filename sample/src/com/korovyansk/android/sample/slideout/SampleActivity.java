25b61f44d3f894fc57b9f8fba4c58fa3a22284b3
Alex Korovyansky <korovyansk@gmail.com>
2012-07-17 01:17:17 +0700

    changed icon for Sample, implemented hide ActionBar for Simple Sample Activity on device >= HoneyComb

--- sample/src/com/korovyansk/android/sample/slideout/SampleActivity.java
+++ sample/src/com/korovyansk/android/sample/slideout/SampleActivity.java
@@ -1,7 +1,9 @@
 package com.korovyansk.android.sample.slideout;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.TypedValue;
 import android.view.View;
@@ -10,10 +12,14 @@ import com.korovyansk.android.slideout.SlideoutActivity;
 
 public class SampleActivity extends Activity {
 
+	@TargetApi(11)
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.sample);
+		if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB){
+	    	getActionBar().hide();
+	    }
 		findViewById(R.id.sample_button).setOnClickListener(
 				new View.OnClickListener() {
 					@Override
